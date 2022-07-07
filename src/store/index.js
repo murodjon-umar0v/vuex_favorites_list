4101a02f110e99dd744df770e3251a924d2f7594
murodjon.umarovv <murodjon.umarovv@gmail.com>
2022-07-07 11:25:56 +0500

    *

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,46 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    liked: [],
+    products: [{
+        id: 1,
+        name: "iPhone X",
+        image: "https://assets.swappie.com/cdn-cgi/image/width=600,height=600,fit=contain,format=auto/swappie-iphone-x-space-gray.png",
+        description: "The best phone ever",
+        is_liked: false,
+      },
+      {
+        id: 2,
+        name: "iPhone XS",
+        image: "https://shop.ee.co.uk/medias/iphone-12-5g-64gb-purple-desktop-detail-1-Format-488?context=bWFzdGVyfHJvb3R8MTQ5MDIyfGltYWdlL3BuZ3xzeXMtbWFzdGVyL3Jvb3QvaGNhL2hlYi85NjQ2MDQ0OTM4MjcwL2lwaG9uZS0xMi01Zy02NGdiLXB1cnBsZS1kZXNrdG9wLWRldGFpbC0xX0Zvcm1hdC00ODh8ZmNmYzNkMDRjYTc4MjYzYWIwNTgyM2ZiYzJmYTg4ZmUzYWU2NDZhZWU2NmI0YzBhMjM0M2VmZjEyNWY4ZWRiZA",
+        description: "The best phone ever",
+        is_liked: false,
+      },
+      {
+        id: 3,
+        name: "iPhone XS Max",
+        image: "https://inventstore.in/wp-content/uploads/2021/09/iPhone-13-Midnight-1.png",
+        description: "The best phone ever",
+        is_liked: false,
+      },
+      {
+        id: 4,
+        name: "iPhone XR",
+        image: "https://shop.ee.co.uk/medias/iphone-13-pro-max-5g-alpine-green-desktop-detail-1-Format-488?context=bWFzdGVyfHJvb3R8MTM2NTE5fGltYWdlL3BuZ3xzeXMtbWFzdGVyL3Jvb3QvaGZhL2gxYS85ODE0NjY4ODM2ODk0L2lwaG9uZS0xMy1wcm8tbWF4LTVnLWFscGluZS1ncmVlbi1kZXNrdG9wLWRldGFpbC0xX0Zvcm1hdC00ODh8NTJjMmUyYWUyMjlkNTFiZGMxMDQ1Y2NlMWVlMWI2M2VkNmM1NTVmNTYzNmU0YmUxMzMwOThlNzIzYzcwZTU0YQ",
+        description: "The best phone ever",
+        is_liked: false,
+      },
+      {
+        id: 5,
+        name: "iPhone 11",
+        image: "https://img.poorvika.com/cdn-cgi/image/width=1600,height=1600,quality=75/1600_JPG/Mobiles/Apple/13-pro/Apple-iPhone-13-Pro-Sierra-Blue-Front-1.png",
+        description: "The best phone ever",
+        is_liked: false,
+      },
+    ],
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
---
+++ src/views/FavouritesPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="favourites">
+    <div>
+      <div class="no-items" v-if="this.$store.state.liked == 0">
+        <img
+          src="https://cdn.dribbble.com/users/12570/screenshots/13987694/media/1635918fab6854e489723a301619b7b2.jpg?compress=1&resize=400x300"
+          alt=""
+        />
+      </div>
+      <div v-else class="product-grid">
+        <h1 style="width: 100%">Favourites</h1>
+        <div
+          class="card"
+          v-for="(product, index) in this.$store.state.liked"
+          :key="index"
+        >
+          <div class="card-image">
+            <img :src="product.image" alt="iPhone" />
+          </div>
+          <div class="card-content">
+            <h3 style="white-spacing: nowrap">
+              {{ product.name }}
+            </h3>
+            <p>{{ product.description }} - {{ product.id }}</p>
+            <div class="card-bottom">
+              <router-link to="/about">About</router-link>
+              <div class="like-icon">
+                <!-- svg solid -->
+                <svg
+                  @click="addToLiked(product)"
+                  v-if="product.is_liked == false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#ff0000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-heart"
+                >
+                  <path
+                    d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+                  ></path>
+                </svg>
+                <svg
+                  @click="product.is_liked = !product.is_liked"
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  fill="#ff0000"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FavouritesPage",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
